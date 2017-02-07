9de62397cb80924e9328568b2597d6a95d63091b
Lars Andre <landas@gmail.com>
2017-02-07 13:06:36 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+cdd
+===
+
+A simple Bash script to create shortcuts for paths in terminal.
+
+How?
+----
+
+1) source /path/to/cdd.sh
+
+2) Type: cdd
+
+Config
+------
+
+Shortcuts are stored in ~/.cdd
+
+Usage
+-----
+
+cdd add <shortcut> <path>
+cdd remove <shortcut>
+cdd list [-v]
+cdd <shortcut>
+
+Contributing
+------------
+I'm happy if someone wants to improve this script.
+E-mail me at landas@gmail.com
+
+License
+-------
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
---
+++ cdd.sh
@@ -0,0 +1,109 @@
+#!/usr/bin/env bash
+
+# bash script for creating path shortcuts in terminal
+# Copyright (C) 2017 Lars Andre Landås <landas@gmail.com>
+# Distributed under the GNU General Public License, version 2.0.
+
+# For usage type cdd in terminal after you have sourced this file
+# Includes auto-complete support
+# Mac fix: edit line stating with "sed -i ..." to "sed -i '' ..."
+
+if [ ! -f ~/.cdd ]; then
+    touch ~/.cdd
+fi
+
+cdd () {
+	
+	cdd_array=() # Create array
+	while IFS= read -r line # Read a line
+	do
+		cdd_array+=("$line") # Append line to the array
+	done < ~/.cdd
+
+	remove () {
+		echo "Removing $1 if exists in ~/.cdd"
+		sed -i "/^$1 /d" ~/.cdd
+	}
+
+	if [[ $1 = "add" ]] && [[ ! $2 = "" ]]; then
+		remove $2
+		echo "$2 $3" >> ~/.cdd
+		echo "Added $2 $3 to ~/.cdd"
+		return
+	fi
+
+	if [[ $1 = "remove" ]]; then
+		remove $2
+		return
+	fi
+
+	if [[ $1 = "list" ]] && [[ $2 = "-v" ]]; then
+		sort ~/.cdd
+		return
+	fi
+
+
+	if [[ $1 = "list" ]]; then
+		b=($(for l in "${cdd_array[@]}"; do echo "$l"|cut -d' ' -f1; done | sort))
+
+		for l in "${b[@]}"; do 
+			printf "%-8s\n" "${l}"
+		done | column	
+		
+		return
+	fi
+
+	for line in "${cdd_array[@]}"
+	do
+		thea=($line)
+		IFS=' ' read -r id path <<< "$line"
+		if [[ $1 == ${id} ]]; then
+			eval cd "'${path}'"
+			return
+		fi
+	done
+
+	if [[ ! $1 == "" ]]; then
+		echo -e "\e[91m\e[1mError:\e[21m\e[39m $1 is not a shortcut"
+	fi
+
+	echo -e "\e[1mUsage:\e[21m cdd <shortcut> | add <shortcut> path | remove <shortcut> | list [-v]"
+}
+
+_cdd() {
+	local cur cdd_array cdd_args thea
+	
+	cur=${COMP_WORDS[COMP_CWORD]}
+
+	COMPREPLY=()
+	cdd_array=() # Create array
+	cdd_args=""
+	while IFS= read -r line # Read a line
+	do
+		cdd_array+=("$line") # Append line to the array
+		thea=($line)
+		cdd_args="$cdd_args ${thea[0]}"
+	done < ~/.cdd
+
+	if [ $COMP_CWORD -eq 1 ]; then
+		COMPREPLY=( $(compgen -W "$cdd_args" -- $cur) )
+	elif [ $COMP_CWORD -eq 2 ]; then
+		case "${COMP_WORDS[COMP_CWORD-1]}" in
+			"remove")
+				COMPREPLY=( $(compgen -W "$cdd_args" -- $cur) )
+				;;
+			"add")
+				COMPREPLY=( $(compgen -f -- $cur) )
+				;;
+		esac
+	elif [ $COMP_CWORD -eq 3 ]; then
+		case "${COMP_WORDS[COMP_CWORD-2]}" in
+			"add")
+				_filedir
+				;;
+		esac
+	fi
+	return 0
+}
+
+complete -F _cdd cdd
